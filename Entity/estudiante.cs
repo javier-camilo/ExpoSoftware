c1646836e5fafa760556a3a76ec2bc1a618a65be
calvoeduer <calvoeduer@gmail.com>
2020-05-13 20:00:46 -0500

    add class Entity/estudiante

---
+++ Entity/estudiante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class estudiante
+    {
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Asignaturas { get; set; }
+        public int Semestre { get; set; }
+    }
+}
