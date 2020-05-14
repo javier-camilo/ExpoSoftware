9406ff6fdbd5ff552da7051bd87da7857783f641
calvoeduer <calvoeduer@gmail.com>
2020-05-13 19:28:43 -0500

    creacion de estructura frontend: estudiante

--- ExpoSoftware/ClientApp/src/app/app.module.ts
+++ ExpoSoftware/ClientApp/src/app/app.module.ts
@@ -39,6 +39,10 @@ import { ProyectoRegistroComponent } from './docenteAsesor/proyecto/proyecto-reg
 import { ProyectoConsultaComponent } from './docenteAsesor/proyecto/proyecto-consulta/proyecto-consulta.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import { EstudianteconsultaComponent } from './comite/estudiante/estudianteconsulta/estudianteconsulta.component';
+import { EstudianteConsultaComponent } from './comite/estudiante/estudiante-consulta/estudiante-consulta.component';
+import { EstudianteRegistroComponent } from './comite/estudiante/estudiante-registro/estudiante-registro.component';
+import { FiltroEstudiantePipe } from './pipe/filtro-estudiante.pipe';
 
 
 
@@ -64,7 +68,11 @@ import {MatSelectModule} from '@angular/material/select';
     FiltroDocentePipe,
     FiltoAsignaturaAreaPipe,
     ProyectoRegistroComponent,
-    ProyectoConsultaComponent
+    ProyectoConsultaComponent,
+    EstudianteconsultaComponent,
+    EstudianteConsultaComponent,
+    EstudianteRegistroComponent,
+    FiltroEstudiantePipe
 
   ],
   imports: [
---
+++ ExpoSoftware/ClientApp/src/app/comite/estudiante/estudiante-consulta/estudiante-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estudiante-consulta',
+  templateUrl: './estudiante-consulta.component.html',
+  styleUrls: ['./estudiante-consulta.component.css']
+})
+export class EstudianteConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
