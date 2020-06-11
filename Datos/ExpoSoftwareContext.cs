96122e964bf50e0de7ef8141e993c5354fb01ee7
javier-camilo <camiloviloriatovar@gmail.com>
2020-06-11 06:51:58 -0500

    cambios

--- Datos/ExpoSoftwareContext.cs
+++ Datos/ExpoSoftwareContext.cs
@@ -12,11 +12,12 @@ namespace Datos
         }
 
         public DbSet<Area> Areas { get; set; }
-
         public DbSet<Asignatura> Asignaturas { get; set; }
         public DbSet<Docente> Docentes { get; set; }
         public DbSet<Proyecto> Proyectos { get; set; }
         public DbSet<Estudiante> Estudiantes {get; set; }
+        public DbSet<AspectoEvaluar> AspectoEvaluars {get; set; }
+        public DbSet<Rubrica> Rubricas {get; set; }
         
     }
 }
