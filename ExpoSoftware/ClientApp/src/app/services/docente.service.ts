3fd74afa42ab8d5d16be5b49c311683478db68f7
javier-camilo <camiloviloriatovar@gmail.com>
2020-06-12 02:02:59 -0500

    nuevos cambios

--- ExpoSoftware/ClientApp/src/app/services/docente.service.ts
+++ ExpoSoftware/ClientApp/src/app/services/docente.service.ts
@@ -40,12 +40,8 @@ export class DocenteService {
 
             if(mostrar!=null){
 
-<<<<<<< HEAD
                 this.handleErrorService.log('Guardado con exito gg')
                 
-=======
-                this.handleErrorService.log('Guardado con exito esto')
->>>>>>> 047ff2d2d787433b876d7b1626fff814776566f2
 
             }
 
