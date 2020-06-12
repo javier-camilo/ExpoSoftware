af0ed451b248eca4ade7818026e64f7e008971b7
javier-camilo <camiloviloriatovar@gmail.com>
2020-06-11 21:30:40 -0500

    first commit

---
+++ Entity/Estudiante.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Entity
+{
+
+    public class Estudiante
+    {
+        
+        [Key]
+        public string Identificacion { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+
+        public string celular { get; set; }
+
+        public string Asignatura { get; set; }
+
+        public int Semestre { get; set; }
+
+    }
+}
---
+++ ExpoSoftware/ClientApp/src/app/docenteAsesor/consulta-solicitudes/consulta-solicitudes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Proyecto } from '../proyecto/models/proyecto';
+import { ProyectoService } from 'src/app/services/proyecto.service';
+
+@Component({
+  selector: 'app-consulta-solicitudes',
+  templateUrl: './consulta-solicitudes.component.html',
+  styleUrls: ['./consulta-solicitudes.component.css']
+})
+export class ConsultaSolicitudesComponent implements OnInit {
+
+  docente:string;
+  proyectos:Proyecto[];
+
+  constructor(private proyectoService:ProyectoService) { }
+
+  ngOnInit(): void {
+
+    
+
+  }
+
+}
