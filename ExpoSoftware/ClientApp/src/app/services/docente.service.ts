99122bff466be04ea0fb39acbe4c6a01e8d3536f
javier-camilo <camiloviloriatovar@gmail.com>
2020-06-11 23:27:27 -0500

    cambios

--- ExpoSoftware/ClientApp/src/app/services/docente.service.ts
+++ ExpoSoftware/ClientApp/src/app/services/docente.service.ts
@@ -40,7 +40,7 @@ export class DocenteService {
 
             if(mostrar!=null){
     
-                this.handleErrorService.log('Guardado con exito esto sdfs')
+                this.handleErrorService.log('Guardado con exito esto sdfs asdas dfsf')
 
             }
 
